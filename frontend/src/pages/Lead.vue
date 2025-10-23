eabf2cc9fac7946a20a0682566e4828ff916663b
@bayanmunkh456 <baynmonkhbattomor11@gmail.com>
2025-10-23 13:41:12 +0800

    added attributes

--- frontend/src/pages/Lead.vue
+++ frontend/src/pages/Lead.vue
@@ -1,263 +1,314 @@
 <template>
-    <a-page-header class="!p-0 !mb-2" title="Lead" @back="() => $router.go(-1)">
-        <template #tags>
-            <a-tag v-if="lead?.isDirty" color="orange">Not saved</a-tag>
-        </template>
-        <template #extra>
-            <a-button @click="oppOpen = true">Create Opportunity</a-button>
-            <a-button @click="createContact">Create contact</a-button>
-            <a-button @click="saveDoc()" :loading="lead?.save?.loading" type="primary">
-                Save
-            </a-button>
-        </template>
-    </a-page-header>
-    <div class="w-full  rounded bg-white p-4">
-        <a-spin :spinning="lead.get.loading"></a-spin>
-        <div v-if="!lead.get.loading">
-            <div class="border-b">
-                <div class="text-xl font-semibold">Customer Details</div>
-                <a-form class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2 !mt-2" layout="vertical">
-                    <a-form-item label="Customer">
-                        <a-input v-model:value="lead.doc.customer" />
-                    </a-form-item>
-                    <a-form-item label="Email">
-                        <a-input v-model:value="lead.doc.email" />
-                    </a-form-item>
-                    <a-form-item label="Company name">
-                        <a-input v-model:value="lead.doc.company_name" />
-                    </a-form-item>
-                    <a-form-item label="Country">
-                        <a-select v-model:value="lead.doc.country" :options="countryOptions"
-                            :loading="countrys.list.loading" placeholder="Select country" :showSearch="true"
-                            :show-arrow="false" option-filter-prop="label" :filter-option="true"
-                            :not-found-content="null" />
-                    </a-form-item>
-                    <a-form-item label="Whatsapp">
-                        <a-input v-model:value="lead.doc.whatsapp" />
-                    </a-form-item>
-                </a-form>
-            </div>
-            <div class="border-b">
-                <div class="text-xl font-semibold mt-2">Lead Details</div>
-                <a-form class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2 !mt-2" layout="vertical">
-                    <a-form-item label="Age (average)">
-                        <a-input v-model:value="lead.doc.age" />
-                    </a-form-item>
-                    <a-form-item :colon="false" labelAlign="left" label="Group size">
-                        <a-space>
-                            <a-input-number v-model:value="lead.doc.group_min" :min="1" :max="20" addon-before="min" />
-                            <a-input-number v-model:value="lead.doc.group_max" :min="1" :max="20" addon-before="max" />
-                        </a-space>
-                    </a-form-item>
-                    <a-form-item label="Comfort type">
-                        <a-select v-model:value="lead.doc.comfort_type" :options="comfortOptions" />
-                    </a-form-item>
-                    <a-form-item label="Season">
-                        <a-input v-model:value="lead.doc.season" />
-                    </a-form-item>
-                    <a-form-item label="Duration">
-                        <a-input v-model:value="lead.doc.duration" />
-                    </a-form-item>
-                    <a-form-item label="Nationality">
-                        <a-input v-model:value="lead.doc.nationality" />
-                    </a-form-item>
-                    <a-form-item label="Group Type">
-                        <a-select v-model:value="lead.doc.group_type" :options="groupTypes" />
-                    </a-form-item>
-                    <a-form-item label="Interest">
-                        <a-textarea v-model:value="lead.doc.interest" :row="3" />
-                    </a-form-item>
-                    <a-form-item label="Year">
-                        <a-checkbox-group v-model:value="leadYear" :options="getYearOptions()" />
-                    </a-form-item>
-                </a-form>
-            </div>
-            <div class="">
-                <div class="text-xl font-semibold mt-2">Notes</div>
-                <a-form class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 !mt-2" layout="vertical">
-                    <a-form-item label="Note">
-                        <a-textarea v-model:value="lead.doc.note" row="6" />
-                    </a-form-item>
-                    <a-form-item label="Follow up Requests">
-                        <a-textarea v-model:value="lead.doc.follow_up_requests" :row="6" />
-                    </a-form-item>
-                    <a-form-item label="Business Cart">
-                        <FileUploader :fileTypes="['jpg', 'jpeg', 'png']" :multiple="false"
-                            @success="(file) => { lead.doc.image = file.file_url }" class="border-none">
-                            <template #default="{ openFileSelector }">
-                                <div class="border rounded-lg overflow-hidden cursor-pointer flex items-center justify-center"
-                                    @click="openFileSelector">
-                                    <img v-if="lead.doc.image" :src="lead.doc.image" alt="avatar"
-                                        class="w-full h-56 object-cover" />
-                                    <div v-else class="min-h-40 flex items-center justify-center text-gray-500 text-sm">
-                                        Upload Image
-                                    </div>
-                                </div>
-                            </template>
-                        </FileUploader>
-
-                    </a-form-item>
-                </a-form>
-            </div>
-        </div>
-    </div>
-    <a-modal title="New Opportunity" v-model:open="oppOpen" okText="Create" @ok="createOpertunity">
-        <a-form :model="oppForm" layout="vertical">
-            <a-form-item label="Assignee" name="assignee">
-                <a-select v-model:value="oppForm.assignee" :options="employeeOptions" placeholder="Select assignee">
-                    <template #option="{ label, value, image }">
-                        <div class="flex items-center gap-2">
-                            <a-avatar :src="image" size="small" />
-                            <div class="flex flex-col leading-tight">
-                                <span>{{ label }}</span>
-                                <span class="text-xs text-gray-400">{{ value }}</span>
-                            </div>
-                        </div>
-                    </template>
-                </a-select>
-            </a-form-item>
-            <a-form-item label="Deadline" name="deadline">
-                <a-date-picker v-model:value="oppForm.deadline" format="YYYY-MM-DD" valueFormat="YYYY-MM-DD"
-                    class="w-full" />
-            </a-form-item>
-            <a-form-item label="Other information" name="other_information">
-                <a-textarea v-model:value="oppForm.other_information" :rows="6"></a-textarea>
-            </a-form-item>
-        </a-form>
-    </a-modal>
+	<a-page-header class="!p-0 !mb-2" title="Lead" @back="() => $router.go(-1)">
+		<template #tags>
+			<a-tag v-if="lead?.isDirty" color="orange">Not saved</a-tag>
+		</template>
+		<template #extra>
+			<a-button @click="oppOpen = true">Create Opportunity</a-button>
+			<a-button @click="createContact">Create contact</a-button>
+			<a-button @click="saveDoc()" :loading="lead?.save?.loading" type="primary">
+				Save
+			</a-button>
+		</template>
+	</a-page-header>
+	<div class="w-full rounded bg-white p-4">
+		<a-spin :spinning="lead.get.loading"></a-spin>
+		<div v-if="!lead.get.loading">
+			<div class="border-b">
+				<div class="text-xl font-semibold">Customer Details</div>
+				<a-form
+					class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2 !mt-2"
+					layout="vertical"
+				>
+					<a-form-item label="Customer" name="customer">
+						<a-input v-model:value="lead.doc.customer" />
+					</a-form-item>
+					<a-form-item label="Email" name="email">
+						<a-input v-model:value="lead.doc.email" />
+					</a-form-item>
+					<a-form-item label="Company name" name="company_name">
+						<a-input v-model:value="lead.doc.company_name" />
+					</a-form-item>
+					<a-form-item label="Country" name="country">
+						<a-select
+							v-model:value="lead.doc.country"
+							:options="countryOptions"
+							:loading="countrys.list.loading"
+							placeholder="Select country"
+							:showSearch="true"
+							:show-arrow="false"
+							option-filter-prop="label"
+							:filter-option="true"
+							:not-found-content="null"
+						/>
+					</a-form-item>
+					<a-form-item label="WhatsApp" name="whatsapp">
+						<a-input v-model:value="lead.doc.whatsapp" />
+					</a-form-item>
+				</a-form>
+			</div>
+			<div class="border-b">
+				<div class="text-xl font-semibold mt-2">Lead Details</div>
+				<a-form
+					class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-2 !mt-2"
+					layout="vertical"
+				>
+					<a-form-item label="Age (average)" name="age">
+						<a-input v-model:value="lead.doc.age" />
+					</a-form-item>
+					<a-form-item
+						:colon="false"
+						labelAlign="left"
+						label="Group size"
+						name="group_size"
+					>
+						<a-space>
+							<a-input-number
+								v-model:value="lead.doc.group_min"
+								:min="1"
+								:max="20"
+								addon-before="min"
+							/>
+							<a-input-number
+								v-model:value="lead.doc.group_max"
+								:min="1"
+								:max="20"
+								addon-before="max"
+							/>
+						</a-space>
+					</a-form-item>
+					<a-form-item label="Comfort type" name="comfort_type">
+						<a-select
+							v-model:value="lead.doc.comfort_type"
+							:options="comfortOptions"
+						/>
+					</a-form-item>
+					<a-form-item label="Season" name="season">
+						<a-input v-model:value="lead.doc.season" />
+					</a-form-item>
+					<a-form-item label="Duration" name="duration">
+						<a-input v-model:value="lead.doc.duration" />
+					</a-form-item>
+					<a-form-item label="Nationality" name="nationality">
+						<a-input v-model:value="lead.doc.nationality" />
+					</a-form-item>
+					<a-form-item label="Group Type" name="group_type">
+						<a-select v-model:value="lead.doc.group_type" :options="groupTypes" />
+					</a-form-item>
+					<a-form-item label="Interest" name="interest">
+						<a-textarea v-model:value="lead.doc.interest" :row="3" />
+					</a-form-item>
+					<a-form-item label="Year" name="leadYear">
+						<a-checkbox-group v-model:value="leadYear" :options="getYearOptions()" />
+					</a-form-item>
+				</a-form>
+			</div>
+			<div class="">
+				<div class="text-xl font-semibold mt-2">Notes</div>
+				<a-form
+					class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 !mt-2"
+					layout="vertical"
+				>
+					<a-form-item label="Note" name="note">
+						<a-textarea v-model:value="lead.doc.note" row="6" />
+					</a-form-item>
+					<a-form-item label="Follow up Requests" name="follow_up_requests">
+						<a-textarea v-model:value="lead.doc.follow_up_requests" :row="6" />
+					</a-form-item>
+					<a-form-item label="Business Cart" name="image">
+						<FileUploader
+							:fileTypes="['jpg', 'jpeg', 'png']"
+							:multiple="false"
+							@success="
+								(file) => {
+									lead.doc.image = file.file_url;
+								}
+							"
+							class="border-none"
+						>
+							<template #default="{ openFileSelector }">
+								<div
+									class="border rounded-lg overflow-hidden cursor-pointer flex items-center justify-center"
+									@click="openFileSelector"
+								>
+									<img
+										v-if="lead.doc.image"
+										:src="lead.doc.image"
+										alt="avatar"
+										class="w-full h-56 object-cover"
+									/>
+									<div
+										v-else
+										class="min-h-40 flex items-center justify-center text-gray-500 text-sm"
+									>
+										Upload Image
+									</div>
+								</div>
+							</template>
+						</FileUploader>
+					</a-form-item>
+				</a-form>
+			</div>
+		</div>
+	</div>
+	<a-modal title="New Opportunity" v-model:open="oppOpen" okText="Create" @ok="createOpertunity">
+		<a-form :model="oppForm" layout="vertical">
+			<a-form-item label="Assignee" name="assignee">
+				<a-select
+					v-model:value="oppForm.assignee"
+					:options="employeeOptions"
+					placeholder="Select assignee"
+				>
+					<template #option="{ label, value, image }">
+						<div class="flex items-center gap-2">
+							<a-avatar :src="image" size="small" />
+							<div class="flex flex-col leading-tight">
+								<span>{{ label }}</span>
+								<span class="text-xs text-gray-400">{{ value }}</span>
+							</div>
+						</div>
+					</template>
+				</a-select>
+			</a-form-item>
+			<a-form-item label="Deadline" name="deadline">
+				<a-date-picker
+					v-model:value="oppForm.deadline"
+					format="YYYY-MM-DD"
+					valueFormat="YYYY-MM-DD"
+					class="w-full"
+				/>
+			</a-form-item>
+			<a-form-item label="Other information" name="other_information">
+				<a-textarea v-model:value="oppForm.other_information" :rows="6"></a-textarea>
+			</a-form-item>
+		</a-form>
+	</a-modal>
 </template>
 <script setup>
-import { createDocumentResource, FileUploader } from 'frappe-ui';
-import { computed, reactive, ref } from 'vue';
-import { CountryStore } from '@/data/country';
-import { message } from 'ant-design-vue';
-import { EmployeeStore } from '../data/Employee';
+import { createDocumentResource, FileUploader } from "frappe-ui";
+import { computed, reactive, ref } from "vue";
+import { CountryStore } from "@/data/country";
+import { message } from "ant-design-vue";
+import { EmployeeStore } from "../data/Employee";
 
-const { employees } = EmployeeStore()
-
+const { employees } = EmployeeStore();
 
 const props = defineProps({
-    name: {
-        type: String,
-        required: true
-    }
-})
+	name: {
+		type: String,
+		required: true,
+	},
+});
 
+const { countrys } = CountryStore();
 
-const { countrys } = CountryStore()
-
-const countryOptions = computed(() =>
-    countrys?.data.map(item => ({
-        label: item.name,
-        value: item.name
-    })) || []
-)
-
+const countryOptions = computed(
+	() =>
+		countrys?.data.map((item) => ({
+			label: item.name,
+			value: item.name,
+		})) || [],
+);
 
 const lead = createDocumentResource({
-    doctype: 'Lead',
-    name: props.name,
-    auto: true,
-    whitelistedMethods: {
-        createOpportunity: 'create_opportunity',
-        createContact: 'create_contact'
-    },
-})
-
+	doctype: "Lead",
+	name: props.name,
+	auto: true,
+	whitelistedMethods: {
+		createOpportunity: "create_opportunity",
+		createContact: "create_contact",
+	},
+});
 
 const groupTypes = [
-    {
-        value: 'Family'
-    },
-    {
-        value: 'Friends'
-    },
-    {
-        value: 'Colleages'
-    },
-]
+	{
+		value: "Family",
+	},
+	{
+		value: "Friends",
+	},
+	{
+		value: "Colleages",
+	},
+];
 const comfortOptions = [
-    {
-        value: 'Budget'
-    },
-    {
-        value: 'Luxury'
-    },
-    {
-        value: 'Comfort'
-    },
-]
+	{
+		value: "Budget",
+	},
+	{
+		value: "Luxury",
+	},
+	{
+		value: "Comfort",
+	},
+];
 
-const getYearOptions = () => lead.doc.year.map(item => item.year);
+const getYearOptions = () => lead.doc.year.map((item) => item.year);
 
 const leadYear = computed({
-    get() {
-        const checked = lead.doc.year?.filter(n => n.checked == 1) || []
-        return checked.map(a => a.year)
-    },
-    set(years) {
-        lead.doc.year.forEach(item => {
-            item.checked = years.includes(item.year) ? 1 : 0;
-        });
-    }
-})
+	get() {
+		const checked = lead.doc.year?.filter((n) => n.checked == 1) || [];
+		return checked.map((a) => a.year);
+	},
+	set(years) {
+		lead.doc.year.forEach((item) => {
+			item.checked = years.includes(item.year) ? 1 : 0;
+		});
+	},
+});
 
 const saveDoc = async () => {
-    try {
-        lead.save.submit()
-        message.success('Successflly saved', 2)
-    }
-    catch (error) {
-        message.error(error.message, 2)
-    }
-}
+	try {
+		lead.save.submit();
+		message.success("Successflly saved", 2);
+	} catch (error) {
+		message.error(error.message, 2);
+	}
+};
 
-const oppOpen = ref(false)
+const oppOpen = ref(false);
 const oppForm = reactive({
-    assignee: '',
-    deadline: '',
-    other_information: '',
-})
+	assignee: "",
+	deadline: "",
+	other_information: "",
+});
 
 const employeeOptions = computed(() => {
-    return employees?.data.map(employee => ({
-        value: employee.name,
-        label: employee.first_name,
-        image: employee.image
-    })) || []
-})
+	return (
+		employees?.data.map((employee) => ({
+			value: employee.name,
+			label: employee.first_name,
+			image: employee.image,
+		})) || []
+	);
+});
 
 const createOpertunity = async () => {
-    try {
-        lead.createOpportunity.submit({
-            assignee: oppForm.assignee,
-            deadline: oppForm.deadline,
-            other_information: oppForm.other_information,
-        })
-        Object.assign(oppForm, {
-            assignee: '',
-            deadline: '',
-            other_information: '',
-        })
-        message.success('Successfully created oppertunity', 2)
-        oppOpen.value = false
-
-    }
-    catch (error) {
-        message.error(error?.message || 'Error on creating opportunity', 2)
-        oppOpen.value = false
-    }
-}
+	try {
+		lead.createOpportunity.submit({
+			assignee: oppForm.assignee,
+			deadline: oppForm.deadline,
+			other_information: oppForm.other_information,
+		});
+		Object.assign(oppForm, {
+			assignee: "",
+			deadline: "",
+			other_information: "",
+		});
+		message.success("Successfully created oppertunity", 2);
+		oppOpen.value = false;
+	} catch (error) {
+		message.error(error?.message || "Error on creating opportunity", 2);
+		oppOpen.value = false;
+	}
+};
 
 const createContact = async () => {
-    try {
-        await lead.createContact.submit()
-        message.success('Successfully created contact', 2)
-    } catch (error) {
-        message.error(error?.message || 'There is a error creating contact', 2)
-    }
-
-}
-
-
+	try {
+		await lead.createContact.submit();
+		message.success("Successfully created contact", 2);
+	} catch (error) {
+		message.error(error?.message || "There is a error creating contact", 2);
+	}
+};
 </script>
--- frontend/src/pages/LeadList.vue
+++ frontend/src/pages/LeadList.vue
@@ -1,104 +1,124 @@
 <template>
-    <a-page-header class="!p-0 !mb-2" title="Lead">
-        <template #extra>
-            <a-button @click="leads.reload()" :loading="leads.list.loading" class="!flex justify-center items-center">
-                <template #icon>
-                    <FeatherIcon name="refresh-ccw" class="size-4" />
-                </template>
-            </a-button>
-            <a-button type="primary" @click="handleCreate">Create lead</a-button>
-        </template>
-    </a-page-header>
-    <a-table :columns="columns" :data-source="leads.data" size="small" :loading="leads.list.loading" :pagination="false"
-        row-key="name" :scroll="{ y: 'calc(100vh - 280px)' }" :customRow="onRow">
-    </a-table>
-    <a-button class="mt-2" @click="leads.next()" :loading="leads.list.loading">load more</a-button>
-    <a-modal v-model:open="open" title="New lead" okText="Create" @ok="createLead">
-        <a-form :model="newLead" :rules="newLeadRules" layout="vertical">
-            <a-form-item label="Customer">
-                <a-input v-model:value="newLead.customer" placeholder="customer name" />
-            </a-form-item>
-            <a-form-item label="Email">
-                <a-input v-model:value="newLead.email" placeholder="Email" />
-            </a-form-item>
-            <a-form-item label="Company name">
-                <a-input v-model:value="newLead.company_name" placeholder="Company Name" />
-            </a-form-item>
-            <a-form-item label="Country">
-                <!-- <a-input v-model:value="newLead.country" placeholder="Country" /> -->
-                <a-select v-model:value="newLead.country" :options="countryOptions" :loading="countrys.list.loading"
-                    placeholder="Select country" :showSearch="true" :show-arrow="false" option-filter-prop="label"
-                    :filter-option="true" :not-found-content="null" />
-            </a-form-item>
-            <a-form-item label="Whatsapp">
-                <a-input v-model:value="newLead.whatsapp" placeholder="whatsapp" />
-            </a-form-item>
-        </a-form>
-    </a-modal>
+	<a-page-header class="!p-0 !mb-2" title="Lead">
+		<template #extra>
+			<a-button
+				@click="leads.reload()"
+				:loading="leads.list.loading"
+				class="!flex justify-center items-center"
+			>
+				<template #icon>
+					<FeatherIcon name="refresh-ccw" class="size-4" />
+				</template>
+			</a-button>
+			<a-button type="primary" @click="handleCreate">Create lead</a-button>
+		</template>
+	</a-page-header>
+	<a-table
+		:columns="columns"
+		:data-source="leads.data"
+		size="small"
+		:loading="leads.list.loading"
+		:pagination="false"
+		row-key="name"
+		:scroll="{ y: 'calc(100vh - 280px)' }"
+		:customRow="onRow"
+	>
+	</a-table>
+	<a-button class="mt-2" @click="leads.next()" :loading="leads.list.loading">load more</a-button>
+	<a-modal v-model:open="open" title="New lead" okText="Create" @ok="createLead">
+		<a-form :model="newLead" :rules="newLeadRules" layout="vertical">
+			<a-form-item label="Customer" name="customer">
+				>
+				<a-input v-model:value="newLead.customer" placeholder="customer name" />
+			</a-form-item>
+			<a-form-item label="Email" name="email">
+				<a-input v-model:value="newLead.email" placeholder="Email" />
+			</a-form-item>
+			<a-form-item label="Company name" name="company_name">
+				<a-input v-model:value="newLead.company_name" placeholder="Company Name" />
+			</a-form-item>
+			<a-form-item label="Country" name="country">
+				<!-- <a-input v-model:value="newLead.country" placeholder="Country" /> -->
+				<a-select
+					v-model:value="newLead.country"
+					:options="countryOptions"
+					:loading="countrys.list.loading"
+					placeholder="Select country"
+					:showSearch="true"
+					:show-arrow="false"
+					option-filter-prop="label"
+					:filter-option="true"
+					:not-found-content="null"
+				/>
+			</a-form-item>
+			<a-form-item label="Whatsapp" name="whatsapp">
+				<a-input v-model:value="newLead.whatsapp" placeholder="whatsapp" />
+			</a-form-item>
+		</a-form>
+	</a-modal>
 </template>
 
 <script setup>
-import { createListResource } from 'frappe-ui';
-import { reactive, ref, computed } from 'vue';
-import { useRouter } from 'vue-router';
-import { message } from 'ant-design-vue'
-import { CountryStore } from '@/data/country';
+import { createListResource } from "frappe-ui";
+import { reactive, ref, computed } from "vue";
+import { useRouter } from "vue-router";
+import { message } from "ant-design-vue";
+import { CountryStore } from "@/data/country";
 
-const { countrys } = CountryStore()
+const { countrys } = CountryStore();
 
-const countryOptions = computed(() =>
-    countrys?.data.map(item => ({
-        label: item.name,
-        value: item.name
-    })) || []
-)
+const countryOptions = computed(
+	() =>
+		countrys?.data.map((item) => ({
+			label: item.name,
+			value: item.name,
+		})) || [],
+);
 
-const router = useRouter()
-const open = ref(false)
+const router = useRouter();
+const open = ref(false);
 
 const newLead = reactive({
-    customer: '',
-    email: '',
-    company_name: '',
-    country: '',
-    whatsapp: '',
-})
-const newLeadRules = {}
+	customer: "",
+	email: "",
+	company_name: "",
+	country: "",
+	whatsapp: "",
+});
+const newLeadRules = {};
 
 const leads = createListResource({
-    doctype: 'Lead',
-    fields: ['name', 'customer', 'email', 'company_name', 'country', 'whatsapp'],
-    auto: true,
-    pageLength: 20,
-})
+	doctype: "Lead",
+	fields: ["name", "customer", "email", "company_name", "country", "whatsapp"],
+	auto: true,
+	pageLength: 20,
+});
 
 const columns = [
-    { title: 'Customer', key: 'customer', dataIndex: 'customer' },
-    { title: 'Email', key: 'email', dataIndex: 'email' },
-    { title: 'Company name', key: 'company_name', dataIndex: 'company_name' },
-    { title: 'Country', key: 'country', dataIndex: 'country' },
-    { title: 'Whatsapp', key: 'whatsapp', dataIndex: 'whatsapp' },
-]
+	{ title: "Customer", key: "customer", dataIndex: "customer" },
+	{ title: "Email", key: "email", dataIndex: "email" },
+	{ title: "Company name", key: "company_name", dataIndex: "company_name" },
+	{ title: "Country", key: "country", dataIndex: "country" },
+	{ title: "Whatsapp", key: "whatsapp", dataIndex: "whatsapp" },
+];
 
 const onRow = (record) => ({
-    onClick: () => {
-        router.push({ name: 'Lead', params: { name: record.name } })
-    }
-})
+	onClick: () => {
+		router.push({ name: "Lead", params: { name: record.name } });
+	},
+});
 
 const handleCreate = async () => {
-    open.value = true
-}
+	open.value = true;
+};
 const createLead = async () => {
-    try {
-        await leads.insert.submit({ ...newLead })
-        message.success('Successfully created lead ', 2)
-    }
-    catch (error) {
-        message.error(error.message || 'Failed to create lead', 2)
-    }
-    finally {
-        open.value = false
-    }
-}
+	try {
+		await leads.insert.submit({ ...newLead });
+		message.success("Successfully created lead ", 2);
+	} catch (error) {
+		message.error(error.message || "Failed to create lead", 2);
+	} finally {
+		open.value = false;
+	}
+};
 </script>
